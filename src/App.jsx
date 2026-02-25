374a9d5bc00a2ce9c59b9da3f799fd12023a7df5
nandhastr <nandhastr2@gmail.com>
2026-02-25 14:16:09 +0700

    modified import about

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@
 import './App.css'
 import Footer from './pages/layouts/footer'
 import Navbar from './pages/layouts/navbar'
-import About from './pages/about'
 import Hero from './pages/components/Hero'
 import Kontak from './pages/Kontak'
 import Pencapaian from './pages/Pencapaian'
+import About from './pages/About';
 
 function App() {
 
